be6f26cc45acc055b1b7d017de8f19af74e1e225
Aaron Ujah <aaronujah@gmail.com>
2023-06-03 09:54:18 +0100

    Initialize the auth service functions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async signUp(): Promise<string> {
+    return 'l';
+  }
+
+  async signIn(): Promise<string> {
+    return 'l';
+  }
+
+  async forgotPassword(): Promise<string> {
+    return 'l';
+  }
+
+  async updatePassword(): Promise<string> {
+    return 'l';
+  }
+}
