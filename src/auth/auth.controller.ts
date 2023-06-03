0d31f8b540504e16ce0a5488929efeb7770d9faa
Aaron Ujah <aaronujah@gmail.com>
2023-06-03 09:48:21 +0100

    Initialize the auth endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Patch, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(): string {
+    return null;
+  }
+
+  @Post('/signin')
+  signIn(): string {
+    return null;
+  }
+
+  @Patch('/forgotPassword')
+  forgotPassword(): string {
+    return 'j';
+  }
+
+  @Patch('/updateMyPassword')
+  updatePassword(): string {
+    return 'j';
+  }
+}
