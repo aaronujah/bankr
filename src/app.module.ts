b5027a66f75dd700f750ac716d15f44ffd5e27d4
Aaron Ujah <aaronujah@gmail.com>
2023-06-03 09:34:13 +0100

    Initialize the auth feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AccountsModule, TransactionsModule],
+  imports: [AccountsModule, TransactionsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
