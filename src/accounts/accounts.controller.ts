cf39cbc8b3ae86c9f5cf1d1e1a749e40b3cbf177
Aaron Ujah <aaronujah@gmail.com>
2023-06-04 08:13:52 +0100

    Add accounts endpoints

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
+import { AccountsService } from './accounts.service';
 
 @Controller('accounts')
-export class AccountsController {}
+export class AccountsController {
+  constructor(private accountService: AccountsService) {}
+
+  @Post('/accounts')
+  createAccount(): Promise<string> {
+    return this.accountService.createAccount();
+  }
+
+  @Post('/accounts/add')
+  addAccount(): Promise<string> {
+    return this.accountService.createAccount();
+  }
+
+  @Get('/accounts')
+  getAllAccounts(): Promise<string> {
+    return this.accountService.createAccount();
+  }
+
+  @Get('/accounts/:accountId')
+  getAccount(): Promise<string> {
+    return this.accountService.createAccount();
+  }
+
+  @Delete('/accounts/:accountId')
+  deleteAccount(): Promise<string> {
+    return this.accountService.createAccount();
+  }
+}
--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AccountsService {}
+export class AccountsService {
+  async createAccount(): Promise<string> {
+    return 'l';
+  }
+
+  async addAccount(): Promise<string> {
+    return 'l';
+  }
+
+  async getAllAccounts(): Promise<string> {
+    return 'l';
+  }
+
+  async getAccount(): Promise<string> {
+    return 'l';
+  }
+
+  async deleteAccount(): Promise<string> {
+    return 'l';
+  }
+}
